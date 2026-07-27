8ee266e7c754fbf16611417b4dac6fe0076b51d8
Suvitk <suvitkankariya2007@gmail.com>
2026-07-27 08:29:48 +0530

    0344-REVERSE-STRING Stats: Time: 0 ms (100%), Space: 27.3 MB (53.46%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int org=s.size();
+      /*  int org=s.size();
         for(int i=s.size()-1;i>=0;i--)
         {
             s.push_back(s[i]);
@@ -9,7 +9,17 @@ public:
         
         s.erase(s.begin(), s.begin()+org);
       
-        
+      we use swapping methid by 2 pointers
+        */
+        int l=0,r=s.size()-1;
+        while(l<=r)
+        {
+            char temp=s[l];
+            s[l]=s[r];
+            s[r]=temp;
+            l++;
+            r--;
+        }
         
     }
 };
