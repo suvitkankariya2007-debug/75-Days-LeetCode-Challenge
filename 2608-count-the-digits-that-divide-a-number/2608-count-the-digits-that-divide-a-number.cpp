0b5e859d5c77141efd38120cc1ea5b12b7595968
Suvitk <suvitkankariya2007@gmail.com>
2026-08-05 23:12:00 +0530

    2608-COUNT-THE-DIGITS-THAT-DIVIDE-A-NUMBER Stats: Time: 0 ms (100%), Space: 7.7 MB (98.28%) - LeetHub

--- 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.cpp
+++ 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.cpp
@@ -7,10 +7,12 @@ public:
         while(num>0)
         {
             rem=num%10;
-            if(temp%rem==0){
+                        if(rem%10 != 0 ){
+                                       if(temp%rem==0){
             c++;
             }
             num/=10;
+                        }
 
 
         }return c;
